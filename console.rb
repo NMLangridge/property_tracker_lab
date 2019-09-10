19a290ecb6310e353a10f8eeea074426330774a4
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-10 15:26:10 +0100

    Lab complete

--- console.rb
+++ console.rb
@@ -1,6 +1,8 @@
 require('pry')
 require_relative('./models/property.rb')
 
+# Property.delete_all()
+
 property1 = Property.new({
   'address' => '29 Bezak Street',
   'value' => 7000,
@@ -15,13 +17,23 @@ property1 = Property.new({
 
   property2 = Property.new({
     'address' => '12 Lahndahn Road',
-    'value' => 400000,
+    'value' => 4000,
     'number_of_bedrooms' => 6,
     'year_built' => 1850,
     'buy_let_status' => 'buy',
     'square_footage' => 20000,
     'build' => 'mansion'
     })
+
+    property2.save()
+    # property2.update()
+    # property1.delete()
+    property1.save()
+
+    # found_property = Property.find_by_id(property1.id)
+
+    found_property = Property.find_by_address(property2.address)
+
 
 properties = Property.all()
 binding.pry
--- models/property.rb
+++ models/property.rb
@@ -7,6 +7,7 @@ class Property
   attr_reader :id
 
   def initialize(options)
+    @id = options['id'].to_i() if options['id']
     @address = options['address']
     @value = options['value'].to_i()
     @number_of_bedrooms = options['number_of_bedrooms'].to_i()
@@ -38,4 +39,53 @@ class Property
     @id = db.exec_prepared("save", values)[0]['id'].to_i()
     db.close()
   end
+
+  def update()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "UPDATE property_tracker SET (address, value, number_of_bedrooms, year_built, buy_let_status, square_footage, build) = (
+    $1, $2, $3, $4, $5, $6, $7
+    ) WHERE id = $8"
+    values = [@address, @value, @number_of_bedrooms, @year_built, @buy_let_status, @square_footage, @build, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
+  end
+
+  def delete()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "DELETE FROM property_tracker WHERE id = $1"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+    db.close()
+  end
+
+  def Property.delete_all()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "DELETE FROM property_tracker"
+    db.prepare("delete_all", sql)
+    db.exec_prepared("delete_all")
+    db.close()
+  end
+
+  def Property.find_by_id(id)
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "SELECT * FROM property_tracker WHERE id = $1"
+    values = [id]
+    db.prepare("select", sql)
+    properties = db.exec_prepared("select", values)
+    db.close()
+    return Property.new(properties[0]);
+  end
+
+  def Property.find_by_address(address)
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "SELECT * FROM property_tracker WHERE address = $1"
+    values = [address]
+    db.prepare("select", sql)
+    properties = db.exec_prepared("select", values)
+    db.close()
+    return Property.new(properties[0]);
+  end
+
 end
